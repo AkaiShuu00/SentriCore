2e27ad711cec188a7c084662aff483e3762e0062
fiomercado <mercadoangelfiona@gmail.com>
2026-09-14 21:37:32 +0800

    admin

--- backend/index.js
+++ backend/index.js
@@ -17,8 +17,8 @@ const reportRoutes = require('./routes/reports');
 const auditRoutes = require('./routes/audit');
 const ocrRoutes = require('./routes/ocr');
 const adminResidentRoutes = require('./routes/adminresidents');
-const adminGuardRoutes = require('./routes/adminGuards');
-const adminReportRoutes = require('./routes/adminReports');
+const adminGuardRoutes = require('./routes/adminguards');
+const adminReportRoutes = require('./routes/adminreport');
 
 // Import db
 require('./config/db');
@@ -45,6 +45,7 @@ app.use('/api/ocr', ocrRoutes);
 app.use('/api/admin', adminResidentRoutes);
 app.use('/api/admin', adminGuardRoutes);
 app.use('/api/admin', adminReportRoutes);
+
 // Health check
 app.get('/', (req, res) => {
   res.json({ message: '✅ SentriCore API is running!' });
--- frontend/resident/src/api.js
+++ frontend/resident/src/api.js
@@ -1,6 +1,6 @@
 import axios from 'axios';
 
-const API = 'http://localhost:3000/api';
+const API = 'http://192.168.100.9:3000/api';  
 
 // Axios instance na may auto-attach ng token
 const api = axios.create({ baseURL: API });
